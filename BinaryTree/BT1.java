cb05a86707d40391497dfea9931fa22bad7dc35b
Sushant-Prasad <sushantprasad77@gmail.com>
2024-10-05 13:58:05 +0530

    tree biuld and tree traversal

--- BinaryTree/BT1.java
+++ BinaryTree/BT1.java
@@ -1,6 +1,7 @@
 //Build Tree
-//TC:-O(n)
+
 package BinaryTree;
+import java.util.*;
 
 public class BT1 {
    static class Node{
@@ -15,7 +16,7 @@ public class BT1 {
   }
    static class Tree{
      static int idx=-1;// initial value will be null for every new root node
-    public Node buildTree(int[] nodes){
+    public static Node buildTree(int[] nodes){//TC:-O(n)
       idx++;
       // for leave node
       if(nodes[idx] == -1){
@@ -28,15 +29,81 @@ public class BT1 {
       return newNode;
 
     }
+    public static void preorder(Node root){ // TC:- O(n)
+      if(root == null){
+        return;
+      }
+      System.out.print(root.data + " ");
+      preorder(root.left);
+      preorder(root.right);
+      
+    }
+    public static void inorder(Node root){ // TC:- O(n)
+      if(root == null){
+        return;
+      }
+      
+      inorder(root.left);
+      System.out.print(root.data + " ");
+      inorder(root.right);
+      
+      
+      
+    }
     
-
+    public static void postorder(Node root){ // TC:- O(n)
+      if(root == null){
+        return;
+      }
+      
+      postorder(root.left);
+      postorder(root.right);
+      System.out.print(root.data + " ");
+      
+      
+    }
+    public static void levelorder(Node root){ //TC:- O(n)
+      if(root==null){
+        return;
+      }
+      Queue<Node> q = new LinkedList<>(); // Queue of nodes
+      q.add(root);
+      q.add(null);
+      while(!q.isEmpty()){
+        Node currNode = q.remove(); // current node
+        if(currNode==null){
+          System.out.println(); // next line for null
+          if(q.isEmpty()){
+            break; // When queue is empty...no need to add null again
+          }else{
+            q.add(null);  
+          }
+        }else{
+          System.out.print(currNode.data+" "); 
+        //check left-subtree
+        if(currNode.left!=null){
+        q.add(currNode.left);
+        }
+        // check right subtree
+        if(currNode.right!=null){
+          q.add(currNode.right);
+        }
+      }
+      }
+    }
   }
   public static void main(String[] args) {
     int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     Tree t = new Tree();
     Node root = t.buildTree(nodes);
-    System.out.println(root.data);
-    
+    //System.out.println(root.data);
+    t.preorder(root);
+    System.out.println();
+    t.inorder(root);
+    System.out.println();
+    t.postorder(root);
+    System.out.println();
+    t.levelorder(root);
   }
   
 }
