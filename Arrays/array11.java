c9a3456636fc88f043749676a07ee066e9be50aa
Sushant-Prasad <sushantprasad77@gmail.com>
2024-10-06 19:22:47 +0530

    optimized kadanes algo

--- Arrays/array11.java
+++ Arrays/array11.java
@@ -4,6 +4,23 @@ package Arrays;
 import java.util.*;
 
 public class array11 {
+    //---------Optimized code-----------------
+    // it can take array with all -ve number
+        public static  int maxSubArray(int[] nums) {
+            int n=nums.length;
+            int cs = 0;// current sum
+            int ms=nums[0];// maximum sum
+            for(int i=0;i<n;i++){
+                if(cs<0){
+                    cs=0;
+                }
+                cs+=nums[i]; // cs can take -ve number too;
+                
+                ms =Math.max(cs,ms);
+            }
+            return ms;
+            
+        }
     public static void kadanes(int a[],int n){
         int i;
         int cs=0;
@@ -21,18 +38,22 @@ public class array11 {
 
     }
      public static void main(String args[]){
-    int i,n;
-    int a[]=new int[50];
-    System.out.println("enter size of array");
-    Scanner sc = new Scanner(System.in);
-    n = sc.nextInt();
-    System.out.println("enter elements of array :");
-    for(i=0;i<n;i++){
-        a[i]= sc.nextInt();
+    // int i,n;
+    // int a[]=new int[50];
+    // System.out.println("enter size of array");
+    // Scanner sc = new Scanner(System.in);
+    // n = sc.nextInt();
+    // System.out.println("enter elements of array :");
+    // for(i=0;i<n;i++){
+    //     a[i]= sc.nextInt();
 
-    }
+    // }
+    int a[]={-5,-7,-3};
     
-   kadanes(a,n);
+//    kadanes(a,n);
+   System.out.println();
+   System.out.println(maxSubArray(a));
+   
 
     
 }
